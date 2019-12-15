5eea955afb45602bbdc413a1d6e28e69cc398563
soni lamkaj <soni.lamkaj@fullscreendigital.ro>
2019-12-15 17:41:45 +0100

    import section immediate reload

--- src/components/home/MovieCard.js
+++ src/components/home/MovieCard.js
@@ -1,35 +1,46 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import { connect } from "react-redux";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 import axios from "axios";
 
-import {
-  importTVShow,
-  fetchTVShows
-} from "../../actions/importsActions";
+import { importTVShow, fetchTVShows } from "../../actions/importsActions";
 
 export class MovieCard extends Component {
+  // onSubmit = e => {
+  //   e.preventDefault();
+  //   this.props.importTVShow(this.props.movie);
+  // };
 
-  onSubmit = e => {
-    this.props.importTVShow(this.props.movie);
-}
+  async onSubmit(e) {
+    e.preventDefault();
+    await this.props.importTVShow(this.props.movie);
+    await this.props.fetchTVShows();
+    debugger;
+  }
 
   render() {
     const { movie } = this.props;
     return (
       <div className="col-md-3 mb-5">
         <div className="card card-body bg-dark text-center h-100">
-          <img className="w-100 mb-2" src={`http://image.tmdb.org/t/p/w185/${movie.poster_path}`} alt="Movie Cover" />
+          <img
+            className="w-100 mb-2"
+            src={`http://image.tmdb.org/t/p/w185/${movie.poster_path}`}
+            alt="Movie Cover"
+          />
           <h5 className="text-light card-title">
             {movie.name} - {movie.first_air_date}
           </h5>
-          <Link className="btn btn-primary" to={'/movie/' + movie.id}>
+          <Link className="btn btn-primary" to={"/movie/" + movie.id}>
             Details
             <i className="fas fa-chevron-right" />
           </Link>
-          <button onClick={this.onSubmit} className="btn btn-primary btn-bg mt-3">
-              ImportData
-            </button>
+          <button
+            onClick={this.onSubmit.bind(this)}
+            className="btn btn-primary btn-bg mt-3"
+          >
+            ImportData
+          </button>
         </div>
       </div>
     );
@@ -38,9 +49,9 @@ export class MovieCard extends Component {
 
 // export default MovieCard;
 
-const mapStateToProps = state => ({
-});
+const mapStateToProps = state => ({});
 
 export default connect(mapStateToProps, {
-  importTVShow
+  importTVShow,
+  fetchTVShows
 })(MovieCard);
